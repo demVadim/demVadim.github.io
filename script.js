c879b985dd6d961d8899cf14d9fac21b62dd150b
Vadim <demmed85@mail.ru>
2020-08-30 09:34:44 +0800

    css new

--- script.js
+++ script.js
@@ -1,7 +1,2 @@
 "use strict";
-const input = prompt('Enter a text', '');
-const messageEL = document.createElement('p');
-messageEL.innerText = input;
 
-const messageBox = document.querySelector('.messages');
-messageBox.append(messageEL);
