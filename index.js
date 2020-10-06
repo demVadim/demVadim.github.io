cd3e5db19b97f1ae7c237e09ad01d945a2f1e121
demVadim <69032237+demVadim@users.noreply.github.com>
2020-10-06 15:54:26 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,22 +1,23 @@
 "use strict";
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight);
-const renderer = new THREE.WebGLRenderer();
+const camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight);
+const renderer = new THREE.WebGLRenderer({antialias: true, alpha: true});
 
 ///
 
 scene.background = new THREE.Color();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
-camera.position.z = 5;
+camera.position.set(1, 1, 4);
+const orbit = new THREE.OrbitControls(camera);
 
 
 ///
 
 
 
-const material1 = new THREE.MeshBasicMaterial({color: 0xC7A5D4});
-const cubeGeometry = new THREE.BoxGeometry(1, 1, 1);
+const material1 = new THREE.MeshNormalMaterial();
+const cubeGeometry = new THREE.BoxGeometry(4, 4, 0.1);
 const cube = new THREE.Mesh(cubeGeometry,material1);
 scene.add(cube);
 
@@ -28,8 +29,6 @@ function animate(){
 	requestAnimationFrame(animate);
 	renderer.render(scene, camera);
 
-	cube.rotation.x += 0.02;
-	cube.rotation.y += 0.02;
 }
 
 animate();
